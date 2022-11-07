edbf0a0439bb1895f405c53464c0e286757cc460
TiffanyLo <a110082042@mail.shu.edu.tw>
2022-11-07 22:52:53 +0800

    期中

---
+++ Assets/Assets/Scripts/Goal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.name == "Goal")
+        {
+            SceneManager.LoadScene("Level2");
+
+        }
+    }
+}
